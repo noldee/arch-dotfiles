004d54a9ba68a122b7612a0a880252e8b411fb05
noldee <188809273+noldee@users.noreply.github.com>
2026-09-17 22:10:05 -0500

    feat: enable waybar autostart on Hyprland launch

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -52,7 +52,7 @@ local menu        = "hyprlauncher"
 -- hl.on("hyprland.start", function () 
 --   hl.exec_cmd(terminal)
 --   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
+hl.exec_cmd("waybar & hyprpaper & firefox")
 -- end)
 
 
